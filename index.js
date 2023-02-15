41f88cce3735a4ff4a99e1f45d841758b3115628
bmnaufaal <bmuhammadopang@gmail.com>
2023-02-15 16:25:54 +0700

    feat: login, logout, movies list done

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 $(document).ready(() => {
+  let access_token = localStorage.getItem("access_token");
+  if (access_token) {
+    isLoggedIn();
+    fetchMovies();
+  }
   $("#login-form").submit(handleLogin);
+  $("#nav-logout").click(handleLogout);
 });
 
 const baseUrl = "http://localhost:3000";
+
+function isLoggedIn() {
+  $("#login-section").hide();
+}
 
 function handleLogin(e) {
   e.preventDefault();
@@ -16,12 +26,54 @@ function handleLogin(e) {
       password,
     },
   })
-    .done((data) => {
+    .done((response) => {
       console.log("Berhasil login");
-      console.log(data);
+      localStorage.setItem("access_token", response.access_token);
     })
     .fail((error) => {
       console.log("Gagal login");
-      console.log(error);
+      console.log(error.responseJSON);
+    });
+}
+
+function handleLogout() {
+  localStorage.removeItem("access_token");
+}
+
+function fetchMovies() {
+  $.ajax({
+    url: baseUrl + "/movies",
+    method: "GET",
+    headers: {
+      access_token: localStorage.getItem("access_token"),
+    },
+  })
+    .done((moviesData) => {
+      console.log("Berhasil get movies data");
+      console.log(moviesData);
+      $("#total-product").text(moviesData.length);
+      moviesData.forEach((movies) => {
+        $("#table-product").append(`
+        <tr>
+            <td>${movies.id}</td>
+            <td>${movies.title}</td>
+            <td>${movies.synopsis}</td>
+            <td>${movies.trailerUrl}</td>
+            <td>
+                <img
+                src="${movies.imgUrl}"
+                class="img-fluid"
+                />
+            </td>
+            <td>${movies.rating}</td>
+            <td>${movies.Genre.name}</td>
+            <td>${movies.Author.username}</td>
+        </tr>
+        `);
+      });
+    })
+    .fail((error) => {
+      console.log("Gagal login");
+      console.log(error.responseJSON);
     });
 }
